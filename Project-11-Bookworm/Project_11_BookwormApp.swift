d9bc58c83677fcead4145d1ae1f22391b52e363a
Luca <luca@weve-got-it.com>
2022-09-05 14:17:07 +0200

    Update and Added files: Creating books with Core Data. The addition of the AddBookView to add a book. Basic UI structure created.

--- Project-11-Bookworm/Project_11_BookwormApp.swift
+++ Project-11-Bookworm/Project_11_BookwormApp.swift
@@ -9,9 +9,12 @@ import SwiftUI
 
 @main
 struct Project_11_BookwormApp: App {
+    @StateObject private var dataController = DataController()
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environment(\.managedObjectContext, dataController.container.viewContext)
         }
     }
 }
