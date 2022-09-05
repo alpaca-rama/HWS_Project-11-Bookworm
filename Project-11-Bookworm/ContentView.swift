51a9727850ea3a0617d7755636b00a4c90498750
Luca <luca@weve-got-it.com>
2022-09-05 12:32:26 +0200

    Initial Commit

---
+++ Project-11-Bookworm/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Project-11-Bookworm
+//
+//  Created by Luca Capriati on 2022/09/05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Project-11-Bookworm/Project_11_BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  Project_11_BookwormApp.swift
+//  Project-11-Bookworm
+//
+//  Created by Luca Capriati on 2022/09/05.
+//
+
+import SwiftUI
+
+@main
+struct Project_11_BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
