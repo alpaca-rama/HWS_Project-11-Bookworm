7caa505c67a0249e6849ec8974cbad4ecbfc2f7d
Luca <luca@weve-got-it.com>
2022-09-06 12:04:26 +0200

    Updated files: Lesson -> Deleting from a Core Data fetch request. Added a deleteBooks function that will delete a book from the list and from storage. Added an .onDelete modifier to the ForEach to allow the swipe to delete functionality. Added a ToolBarItem to have an edit button.

--- Project-11-Bookworm/ContentView.swift
+++ Project-11-Bookworm/ContentView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     @Environment(\.managedObjectContext) var moc
+    // Sort FetchRequest with the use of SortDescriptor. Can have more than one sort and will work in order of code.
     @FetchRequest(sortDescriptors: [
         SortDescriptor(\.title),
         SortDescriptor(\.author)
@@ -35,9 +36,14 @@ struct ContentView: View {
                         }
                     }
                 }
+                .onDelete(perform: deleteBooks)
             }
             .navigationTitle("Bookworm")
             .toolbar {
+                ToolbarItem(placement: .navigationBarLeading) {
+                    EditButton()
+                }
+                
                 ToolbarItem(placement: .navigationBarTrailing) {
                     Button {
                         showingAddScreen.toggle()
@@ -50,6 +56,16 @@ struct ContentView: View {
                 AddBookView()
             }
         }
+    }
+    
+    func deleteBooks(at offsets: IndexSet) {
+        for offset in offsets {
+            let book = books[offset]
+            moc.delete(book)
+        }
+        
+        //  comment this line out if you wish to test the edit and swipe to delete action without deleting the content from storage.
+        try? moc.save()
     }
 }
 
